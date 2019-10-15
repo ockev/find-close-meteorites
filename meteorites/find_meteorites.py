27a0b75e8a6e9e1bceaf6538d7965f45a80fe42d
Kevin O'Connor <koconn89@gmail.com>
2019-10-15 15:34:33 -0400

    Don't run script code when imported

--- meteorites/find_meteorites.py
+++ meteorites/find_meteorites.py
@@ -18,19 +18,20 @@ def calc_dist(lat1, lon1, lat2, lon2):
 def get_dist(meteor):
     return meteor.get('distance', math.inf)
 
-my_loc = (40.801941, -73.953072)
+if __name__ == '__main__':
+    my_loc = (40.801941, -73.953072)
 
-# Gets data from NASA API
-meteor_resp = requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
-meteor_data = meteor_resp.json()
+    # Gets data from NASA API
+    meteor_resp = requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
+    meteor_data = meteor_resp.json()
 
-for meteor in meteor_data:
-    if not ('reclat' in meteor and 'reclong' in meteor): continue
-    meteor['distance'] = calc_dist(float(meteor['reclat']),
-                                   float(meteor['reclong']),
-                                   my_loc[0],
-                                   my_loc[1])
+    for meteor in meteor_data:
+        if not ('reclat' in meteor and 'reclong' in meteor): continue
+        meteor['distance'] = calc_dist(float(meteor['reclat']),
+                                       float(meteor['reclong']),
+                                       my_loc[0],
+                                       my_loc[1])
 
-meteor_data.sort(key=get_dist)
+    meteor_data.sort(key=get_dist)
 
-print(meteor_data[0:10])
+    print(meteor_data[0:10])
